a0b126b2298bc154a072791f55a4afd86795ac68
Obo Agboghidi <oboagboghidi@sysadmins-Mac-mini.local>
2019-09-13 16:57:48 -0400

    reddux practice. Initial commit. Expand app size from here

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore } from 'redux';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const initialState = {
+    count: 0
+};
+
+function reducer(state = initialState, action) {
+    switch(action.type){
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMMENT':
+            return {
+                count: state.count - 1
+            };
+        defaullt:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
